f4e03b78c16c312bbda1dca87a688f95806e60dd
Ryan C <ryan2.crawley@live.uwe.ac.uk>
2023-02-05 13:19:45 +0000

    Power up behavior + start of audio transition boundaries

--- Assets/Scripts/Mechanics/Power Ups/PowerUpBehavior_Boots.cs
+++ Assets/Scripts/Mechanics/Power Ups/PowerUpBehavior_Boots.cs
@@ -8,6 +8,8 @@ public class PowerUpBehavior_Boots : MonoBehaviour
     public BoxCollider2D boxCollider;
     public SpriteRenderer sprite;
     private bool isActive = true;
+    public bool has_shoes;
+   
 
 
 
@@ -27,16 +29,22 @@ public class PowerUpBehavior_Boots : MonoBehaviour
         //Destroy the coin if Object tagged Player comes in contact with it
         if (c2d.CompareTag("Player"))
         {
+            c2d.GetComponent<PlayerController>().ActivateDoubleJump();
+            has_shoes = true;
             audioSource.Play();
             isActive = false;
             collider.enabled = GetComponent<Renderer>().enabled = false;
+            boxCollider.enabled = GetComponent<BoxCollider2D>().enabled = false;
+            
+
+
         }
     }
     void Update()
     {
         if (isActive)
         {
-
+          //  Debug.Log(has_shoes);
         }
         else
         {
